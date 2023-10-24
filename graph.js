774e91add25fb5f41373112929ffebbfed5d9ae4
Ahmad Hassan <a2h.ahmadhassan@gmail.com>
2023-10-24 20:23:47 +0500

    solution: Implement Graph Data Structure
    Details:
    * use of factory function to implement required fields and methods.
    * adjacency list used to store connections between vertices.
    * avoided using class to test knowledge of factory functions.

---
+++ graph.js
@@ -0,0 +1,59 @@
+// Implement Graph in js using adjacency list
+// The graph is considered to be a bi-directional Graph
+
+function createGraph() {
+  let connections = [];
+  let totalVertices = 0;
+  let edges = 0;
+
+  const addEdge = (v1, v2) => { // v1 stand for vertex1
+    if (!connections.some(vertex => vertex.vertex === v1)) {
+      addVertex(v1);
+    }
+    if (!connections.some(vertex => vertex.vertex === v2)) {
+      addVertex(v2);
+    }
+
+    let vertex1 = connections.find(vertex => vertex.vertex === v1);
+    let vertex2 = connections.find(vertex => vertex.vertex === v2);
+
+    vertex1.neighbors.push(v2);
+    vertex2.neighbors.push(v1);
+    edges += 2;
+    console.log('Edge Inserted');
+  }
+
+  const addVertex = (v) => {
+    let vertex = {
+      vertex: v,
+      neighbors: [],
+    };
+
+    console.log(`Node: ${v} inserted to graph`);
+    connections.push(vertex);
+    totalVertices++;
+  }
+
+  const displayGraph = () => {
+    console.table(connections);
+  }
+
+  return {
+    addEdge, 
+    displayGraph,
+  };
+}
+
+const G1 = createGraph();
+
+G1.addEdge(15, 100);
+G1.addEdge(15, 11);
+G1.addEdge(15, 17);
+G1.addEdge(100, 50);
+G1.addEdge(50, 11);
+G1.addEdge(50, 17);
+G1.addEdge(17, 19);
+
+G1.displayGraph();
+
+export default createGraph;
